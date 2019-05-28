99820d1c440edbff0c929c756abfaa47a0dc6cb0
amandadlazaro <amanda.lima@itlab.com.br>
2019-05-28 11:59:04 -0300

    feat: use case health check

--- src/domain/usecases/healthCheck/index.ts
+++ src/domain/usecases/healthCheck/index.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize-database';
 import { Auth } from '../../../infra/auth';
-import { SiscofConnector } from '../../../infra/siscof';
 import testPostgresConnectionUseCase from './testPostgresConnectionUseCase';
-import testOracleConnectionUseCase from './testOracleConnectionUseCase';
 import testKeyCloakAccessUseCase from './testKeyCloakAccessUseCase';
 
 export enum healthCheckServicesEnum {
@@ -13,19 +11,16 @@ export enum healthCheckServicesEnum {
 
 export interface HealthCheckUseCases {
   testPostgresConnectionUseCase?: ReturnType<typeof testPostgresConnectionUseCase>;
-  testOracleConnectionUseCase?: ReturnType<typeof testOracleConnectionUseCase>;
   testKeyCloakAccessUseCase?: ReturnType<typeof testKeyCloakAccessUseCase>;
 }
 
 export function getHealthCheckUseCases(
   db: Sequelize,
-  siscofConnector: SiscofConnector,
   auth: Auth
 ) {
   const usecases: HealthCheckUseCases = {};
 
   usecases.testPostgresConnectionUseCase = testPostgresConnectionUseCase(db);
-  usecases.testOracleConnectionUseCase = testOracleConnectionUseCase(siscofConnector);
   usecases.testKeyCloakAccessUseCase = testKeyCloakAccessUseCase(auth);
   return usecases;
 }
